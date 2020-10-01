4e8b613e06f0c77637b6aaf25dcfb652ecfacded
apprenticefarter <63342380+apprenticefarter@users.noreply.github.com>
2020-10-01 15:13:55 +0300

    first commit

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,72 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
+
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+
+
+        for (int i = 0; i < storage.length; i++) {
+
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
+
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+            }
+        }
+        for (int i = 0; i < storage.length - 1; i++) {
+
+            if (storage[i] == null) {
+                storage[i] = storage[i + 1];
+                storage[i + 1] = null;
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int count = 0;
+        for (Resume r : storage) {
+            if (r != null) {
+                count++;
+            }
+        }
+        Resume[] onlyresume = new Resume[count];
+        for (int i = 0; i < onlyresume.length; i++) {
+            onlyresume[i] = storage[i];
+        }
+        return onlyresume;
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
