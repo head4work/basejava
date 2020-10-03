075fe3df51c75dfab55f9d2eb65a08574c5979f1
apprenticefarter <63342380+apprenticefarter@users.noreply.github.com>
2020-10-03 13:25:38 +0300

    Optimized storage.length

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,10 +4,18 @@
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     public int size = 0;
+    public int optimized = optSize();
+
+    private int optSize() {
+        if (size != 0) {
+            return size;
+        }
+        return storage.length;
+    }
 
     void clear() {
         size = 0;
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < optimized; i++) {
             if (storage[i] != null) {
                 storage[i] = null;
             }
@@ -15,7 +23,7 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < optimized; i++) {
             if (storage[i] == null) {
                 storage[i] = r;
                 size++;
@@ -25,7 +33,7 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < optimized; i++) {
             if (storage[i] != null && storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
@@ -35,13 +43,13 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int indexOfDel = 0;
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < optimized; i++) {
             if (storage[i] != null && storage[i].uuid.equals(uuid)) {
                 indexOfDel = i;
                 size--;
             }
         }
-        for (int i = indexOfDel; i < storage.length - 1; i++) {
+        for (int i = indexOfDel; i < optimized - 1; i++) {
             storage[i] = storage[i + 1];
             storage[i + 1] = null;
         }
