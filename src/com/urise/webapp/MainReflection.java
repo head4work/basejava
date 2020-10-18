96f5f9b31ffc31b27e13591e8dd1fa44ffc7ff1d
paperfart <72076525+paperfart@users.noreply.github.com>
2020-10-18 17:03:54 +0300

    invoke toString from Resume clas

---
+++ src/com/urise/webapp/MainReflection.java
@@ -0,0 +1,37 @@
+package com.urise.webapp;
+
+import com.urise.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Collections;
+
+public class MainReflection {
+
+    public static void main(String[] args) throws IllegalAccessException {
+        Resume r = new Resume();
+        Resume r1 = new Resume("123");
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r,"new_uuid");
+        System.out.println(r);
+        Method[] methods = r.getClass().getDeclaredMethods();
+        for (Method method: methods){
+            System.out.println(method.getName());
+            /*Parameter[] parametrs = method.getParameters();
+            for(Parameter parameter:parametrs){
+                System.out.println(parameter.getName());
+                System.out.println(parameter.getType().getName());
+            }*/
+        }
+        try {
+            System.out.println(methods[2].invoke(r, (Object[]) null));
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
