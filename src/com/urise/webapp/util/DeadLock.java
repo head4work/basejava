9d0f8e1b643cd2bd411b40832863c937c1328661
apprenticefarter <63342380+apprenticefarter@users.noreply.github.com>
2021-01-09 17:25:12 +0300

    dead lock optimisation

--- src/com/urise/webapp/util/DeadLock.java
+++ src/com/urise/webapp/util/DeadLock.java
@@ -5,29 +5,15 @@ public class DeadLock {
     private static final Object LOCK1 = new Object();
 
     public static void main(String[] args) {
-
-        Thread thread1 = new Thread(() -> {
-            lock(LOCK, LOCK1);
-        });
-        thread1.start();
-
-        Thread thread2 = new Thread(() -> {
-            lock(LOCK1, LOCK);
-        });
-        thread2.start();
-
+        new Thread(() -> lock(LOCK, LOCK1)).start();
+        new Thread(() -> lock(LOCK1, LOCK)).start();
     }
 
     private static void lock(Object o1, Object o2) {
         synchronized (o1) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            System.out.println("locked on Object " + o1);
             synchronized (o2) {
-                double result = Math.sin(13.0);
-                System.out.println(result);
+                System.out.println("locked on Object " + o1 + ", locking on Object " + o2);
             }
         }
 
